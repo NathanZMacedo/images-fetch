73dd8e607d59aaffccb12f37ad5d8fe13ae867d6
nathanzmacedo <nathanzmacedo@gmail.com>
2023-05-04 16:32:39 -0300

    arrumando bug

--- post.js
+++ post.js
@@ -2,16 +2,16 @@ const form = document.getElementById("my-form");
 form.addEventListener("submit",handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData();
-    data.append("post[title]", event.target.title.value);
-    submitToAPI(data);
+  event.preventDefault();
+  const data = new FormData();
+  data.append("post[title]",event.target.title.value);
+  submitToAPI(data);
 }
 async function submitToAPI(data){
-    var responde = await fetch("http://localhost:3000/posts", {
-        method: "POST",
-        body: data,
-    })
-    var json = await responde.json()
-    console.log(json)
+  var response = await fetch("http://localhost:3000/posts",{
+    method: "POST",
+    body: data,
+  })
+  var json = await response.json()
+  console.log(json)
 }
